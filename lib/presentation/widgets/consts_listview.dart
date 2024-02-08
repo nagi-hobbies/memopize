54a91c6f6af0fe745f41b40e58553ae9f0079748
nagi <99517580+nagi-hobbies@users.noreply.github.com>
2024-02-08 14:16:08 +0900

    Update consts_listview.dart

--- lib/presentation/widgets/consts_listview.dart
+++ lib/presentation/widgets/consts_listview.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:memopize/presentation/widgets/digit.dart';
 import 'package:memopize/presentation/widgets/digits_row.dart';
 
 class ConstsListView extends HookWidget {
